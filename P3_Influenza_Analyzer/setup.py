ef74785883939d28d9d3edd19d55eb7ee8400cb9
sgomezpa1221 <sgomezpa@unal.edu.co>
2025-03-04 06:49:08 -0500

    setup.py

--- P3_Influenza_Analyzer/setup.py
+++ P3_Influenza_Analyzer/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="InfluenzaAnalysis",
+    name="Influenza_Analyzer",
     version="1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
